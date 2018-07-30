72e6e65a36da652f0ce225d2f95cce93b98720b5
Yury Kuryzko <kuryzko25@icloud.com>
2018-07-30 21:48:16 +0300

    Lab 7_1 added.

---
+++ Lab7_1/Lab7_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[20];
+            numbers[0] = 3; 
+            double s = 0;
+            Console.Write(numbers[0] + " ");
+            for (int i = 1; i < 20; i++)
+            {
+                numbers[i] = numbers[i - 1] + 2.3;
+                Console.Write(numbers[i] + " ");
+            }
+            for (int i = 2; i < 14; i++ )
+            {
+                s = s + numbers[i];
+            };
+            Console.WriteLine();
+            Console.WriteLine("Сумма диапазона элементов: {0:0.##}", s);
+            Console.ReadLine();
+        }
+    }
+}
