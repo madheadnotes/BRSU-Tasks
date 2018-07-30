c5ebfcc5821631548350de744a88c91c417fcfa6
Yury Kuryzko <kuryzko25@icloud.com>
2018-07-30 23:12:00 +0300

    Lab 8_1 added.

---
+++ Lab8_1/Lab8_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во элементов массива: ");
+            int q = int.Parse(Console.ReadLine());
+            int[] numbers = new int[q];
+            Random rnd = new Random();
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(-8, -3);
+                sum = sum + numbers[i];
+            }
+            int a = sum / q; bool answer = false;
+            foreach (int s in numbers)
+            {
+                Console.Write(s.ToString() + " ");
+                if (s == a) { answer = true; };
+            }
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическое элементов массива: {0}", a);
+            if (answer == true) { Console.WriteLine("В данном массиве имеется число, равное среднему арифметическому элементов массива."); }
+            else { Console.WriteLine("В данном массиве нет числа, равного среднему арифметическому элементов массива."); };
+            Console.ReadLine();
+        }
+    }
+}
