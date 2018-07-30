b0f68f659b487637cab41c0aac3425f4327cdb09
Yury Kuryzko <kuryzko25@icloud.com>
2018-07-30 20:46:31 +0300

    Lab 1_3 added.

---
+++ Lab1_3/Lab1_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диагональ квадрата:");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Площадь квадрата равна: {0:0.####}", Math.Pow(d, 2) / 2);
+            Console.WriteLine("Периметр квадрата равен: {0:0.####}", d * 2 * Math.Sqrt(2));
+            Console.ReadLine();
+        }
+    }
+}
